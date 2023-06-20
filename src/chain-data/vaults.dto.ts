023e27feb231ac28bcbd4e6002b9ca9e8bb64252
maggo <me@mar.co.de>
2023-06-20 17:46:41 +0200

    fix: add None vault type

--- src/chain-data/vaults.dto.ts
+++ src/chain-data/vaults.dto.ts
@@ -3,6 +3,7 @@ import { AddressSchema, HexSchema, Uint256Schema } from "@astariaxyz/sdk";
 import { PaginationParamsSchema, BoolParamSchema } from "../common";
 
 export enum VaultType {
+  None = "0",
   Solo = "1",
   Public = "2",
 }
